151f316a5f4ce3f731644e1d48699b3a6584f866
Nicolás <nicolas.iloyolar@gmail.com>
2025-03-11 01:25:25 -0300

    cambia configuracion en settings.py

--- digitalizacion/digitalizacion/settings.py
+++ digitalizacion/digitalizacion/settings.py
@@ -85,12 +85,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    BASE_DIR / 'staticfiles',
 ]
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
